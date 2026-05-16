7d2c165027febabe582bbec8dd1de53289fe05d3
Wempt23 <aydossaitov@gmail.com>
2026-05-16 13:31:03 +0500

    51-Number

---
+++ win10.py
@@ -0,0 +1,10 @@
+my_os = "windows_10"
+ram_gb = int(input("How much RAM you got? "))
+if ram_gb == 8:
+     print("(мне лень придумать ещё одну фразу, так что просто держи этот текст)")
+elif ram_gb <= 8:
+    print("Your PC is a shithole, but for python OK")
+elif ram_gb >= 8:
+    print("He has a RAM! Catch him!")
+else:
+    print("ayoo man wtf")
