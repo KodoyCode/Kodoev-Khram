a8211cc476c558db4752098422cfca2034c9a75e
Wempt23 <aydossaitov@gmail.com>
2026-05-19 13:35:24 +0500

    updated stone

--- win10.py
+++ win10.py
@@ -4,6 +4,8 @@ if ram_gb == 8:
      print("(мне лень придумать ещё одну фразу, так что просто держи этот текст)")
 elif ram_gb <= 8:
     print("Your PC is a shithole, but for python OK")
+elif ram_gb <= 4:
+    print("Windows XP")
 elif ram_gb >= 8:
     print("He has a RAM! Catch him!")
 else:
