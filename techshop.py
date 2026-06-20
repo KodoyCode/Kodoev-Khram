aadb4fb76f71a81e25e76b2a0cb62040200b21c2
Kodoy <dshitinina@gmail.com>
2026-06-20 15:57:09 +0500

    updated the techshop stone

--- techshop.py
+++ techshop.py
@@ -4,16 +4,20 @@ tehnika={
 }
 
 while True:
+    print(f"List of items in the shop: {tehnika}")
     item=input("write new item name: ")
     if item=="exit":
         print("exiting..")
         break
+    elif item in tehnika:
+        del tehnika[item]
+        print(f"removed item from shop: {item}")
     else:
         item_price=input("write item price: ")
         price = int(item_price)
 
         tehnika[item]=price
-        print(f'added {item}, price {price}')
+        print(f'added: {item}, price: {price}')
 
         print(tehnika)
 
