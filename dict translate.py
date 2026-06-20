56fdb8e8d953f356bf66cf1d1599342163323ca1
Kodoy <dshitinina@gmail.com>
2026-06-20 15:21:12 +0500

    more dictionary stones

---
+++ dict translate.py
@@ -0,0 +1,12 @@
+sozlik={
+    "apple":"alma",
+    "book": "kitap"
+}
+
+
+print("aldingi sozlik", sozlik)
+
+jana_soz=input("Ingilizshe sozdi jazin: ")
+awdarmasi=input("Onin awdarmasin jazin: ")
+sozlik[jana_soz]=awdarmasi
+print("Janalangan sozlik: ", sozlik)
