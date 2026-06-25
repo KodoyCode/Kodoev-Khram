21a75c7088945757ceade5e3ab68f4d4e09eeaa5
Kodoy <dshitinina@gmail.com>
2026-06-25 15:03:57 +0500

    updated if a b

--- if a b.py
+++ if a b.py
@@ -2,5 +2,7 @@ a=int(input("San kirit"))
 b=int(input("San kirit"))
 if a>b:
     print("a b dan katta")
+elif a==b:
+    print("a b ga teng")
 else:
-    print("a b dan kichik yoki teng")
+    print("a b dan kichik")
