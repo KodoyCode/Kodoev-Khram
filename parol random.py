b2d9152b7e4c214b0a925304bf5d81af413dcbde
Kodoy <dshitinina@gmail.com>
2026-06-27 15:25:59 +0500

    Yanchik and random password generator stones

--- parol random.py
+++ parol random.py
@@ -16,7 +16,7 @@ input,append,extend,sort while...
 '''
 import random
 
-# Буквы (arip)
+
 a = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -39,7 +39,7 @@ vse.extend(c)
 
 vse.sort()
 
-print("--- OTP Generator Baslandi ---")
+print("generator")
 
 while True:
     spisok = []
---
+++ yan.py
@@ -0,0 +1,9 @@
+aydos= -1488
+yanchik=14888888888
+if 2%2==0:
+    aydos+=yanchik
+    yanchik=0
+    print(aydos)
+    print(yanchik)
+else:
+    print("Y")
