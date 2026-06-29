f5942b9427cdb10a6f1fd4678dfffdb42ced8e5e
Aydos Saiytov <kodoy.code@proton.me>
2026-06-29 10:58:02 +0500

    Dulok

---
+++ Lord Farquuauauaaauauauauuad 2.py
@@ -0,0 +1,11 @@
+import time
+import random
+Lord_Farquauauauauauuauaaaauuuuuauauauuauauad=[]
+Fairytale_creatures_and_motherfuckers=["Fairy_Godfather","Pinocchio","Granny_wolf","Princess_Fiona","Lord_Fuckwad","Fucking_Sexy_Dragon","Troll","Shrek","Reddit_Mod","Three_Bears","Aleksandr_Sergeevich_Pushka","Stinky Pony","Dude"]
+while Fairytale_creatures_and_motherfuckers:
+    magic_random = random.choice(Fairytale_creatures_and_motherfuckers)
+    Lord_Farquauauauauauuauaaaauuuuuauauauuauauad.append(magic_random)
+    Fairytale_creatures_and_motherfuckers.remove(magic_random)
+    print(f"Lord_Farquauauauauauuauaaaauuuuuauauauuauauad: {Lord_Farquauauauauauuauaaaauuuuuauauauuauauad}")
+    print(f"Fairytale_creatures: {Fairytale_creatures_and_motherfuckers}")
+    time.sleep(120)
---
+++ Trash Can/Lord Farquuauauaaauauauauuad.py
@@ -0,0 +1,11 @@
+import time
+import random
+Lord_Farquauauauauauuauaaaauuuuuauauauuauauad=[]
+Fairytale_creatures=["Fairy_Godmother","Pinocchio","Granny_wolf","Princess_Fiona","Lord_Fuckwad","Fucking_Sexy_Dragon","Troll","Shrek","Reddit_Mod","Three_Bears","Aleksandr_Sergeevich_Pushka"]
+while True:
+    magic_random = random.choice([Fairytale_creatures])
+    Lord_Farquauauauauauuauaaaauuuuuauauauuauauad += magic_random
+    Fairytale_creatures - magic_random
+    print(f"Lord_Farquauauauauauuauaaaauuuuuauauauuauauad: {Lord_Farquauauauauauuauaaaauuuuuauauauuauauad}")
+    print(f"Fairytale_creatures: {Fairytale_creatures}")
+    time.sleep(120)
